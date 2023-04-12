3456632e47fbcd2743e8361cf278702c9c52cb06
xiaochen <2011004661@qq.com>
2023-04-13 01:00:12 +0800

    Site updated: 2023-04-13 01:00:12

--- js/anzhiyufunction.js
+++ js/anzhiyufunction.js
@@ -1,1 +1,9 @@
-<link rel="stylesheet" class="aplayer-secondary-style-marker" href="/assets/css/APlayer.min.css"><script src="/assets/js/APlayer.min.js" class="aplayer-secondary-script-marker"></script><script class="meting-secondary-script-marker" src="/assets/js/Meting.min.js"></script>
+<link rel="stylesheet" class="aplayer-secondary-style-marker" href="/assets/css/APlayer.min.css"><script src="/assets/js/APlayer.min.js" class="aplayer-secondary-script-marker"></script><script class="meting-secondary-script-marker" src="/assets/js/Meting.min.js"></script>// 音乐绑定事件
+function musicBindEvent() {
+  document.querySelector("#nav-music .aplayer-music").addEventListener("click", function () {
+    anzhiyu.musicTelescopic();
+  });
+  document.querySelector("#nav-music .aplayer-button").addEventListener("click", function () {
+    anzhiyu.musicToggle(false);
+  });
+}
