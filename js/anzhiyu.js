4dbd27cad99160376257c99913ec8ffdcc51d7dc
xiaochen <2011004661@qq.com>
2023-04-17 00:18:20 +0800

    Site updated: 2023-04-17 00:18:20

--- js/anzhiyu.js
+++ js/anzhiyu.js
@@ -1,4 +1,4 @@
-<link rel="stylesheet" class="aplayer-secondary-style-marker" href="/assets/css/APlayer.min.css"><script src="/assets/js/APlayer.min.js" class="aplayer-secondary-script-marker"></script><script class="meting-secondary-script-marker" src="/assets/js/Meting.min.js"></script>var navMusicEl = document.getElementById("nav-music");
+<link rel="stylesheet" class="aplayer-secondary-style-marker" href="/css/APlayer.min.css"><script src="/js/APlayer.min.js" class="aplayer-secondary-script-marker"></script><script class="meting-secondary-script-marker" src="/js/Meting.min.js"></script>var navMusicEl = document.getElementById("nav-music");
 var anzhiyu = {
   //切换音乐播放状态
   musicToggle: function (changePaly = true) {
